4c139b90350a06e78dfe97ccf69c08de6abcfd89
1e10 <sample@bths.edu>
2022-10-11 10:57:45 -0400

    new lab

---
+++ src/main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+
+        String greeting = "Hello, my name is Amanda!";
+        String info = "I'm a junior in the software engineering major.";
+
+        String cut = greeting.substring(7);
+        System.out.println(cut);
+        String grade = info.substring(0,12);
+        System.out.println(grade);
+    }
+}
